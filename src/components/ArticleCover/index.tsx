19266450090ebd13c9fc5121573fcc51177f620c
Yanis Sadi <dev@sadio.world>
2022-09-14 00:50:42 +0200

    first version of home page without search bar, next step go api

--- src/components/ArticleCover/index.tsx
+++ src/components/ArticleCover/index.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import {
     Card,
     CardMedia,
-    CardContent
+    Box,
+    Typography
 } from "@mui/material";
+import './index.scss';
 
 const ArticleCover = (props: any) => {
     return (
-        <Card sx={{ maxWidth: 500 }}>
+        <Card sx={{ maxWidth: 500 }} className="article-cover">
             <CardMedia
                 component="img"
                 alt="vestiaire"
                 image="https://cdn.paris.fr/qfapv4/2021/11/19/huge-2fbe0719e8a334a12114b76b2bb93cbc.jpg"
             />
+            <Box className="article-cover-fade">
+                <Typography variant="h6" fontFamily={"Londrina Solid"} className="article-cover-text">
+                    Visite de Paris en trottinette et chasse au trésor !
+                </Typography>
+            </Box>
         </Card>
     )
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -9,12 +9,22 @@ const theme = createTheme({
 });
 
 theme.typography.h3 = {
+  fontSize: "1.5rem",
+  "@media (min-width:600px)": {
+    fontSize: "1.8rem",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "2.4rem",
+  },
+};
+
+theme.typography.h6 = {
   fontSize: "1.2rem",
   "@media (min-width:600px)": {
     fontSize: "1.5rem",
   },
   [theme.breakpoints.up("md")]: {
-    fontSize: "2.4rem",
+    fontSize: "1.8rem",
   },
 };
 
